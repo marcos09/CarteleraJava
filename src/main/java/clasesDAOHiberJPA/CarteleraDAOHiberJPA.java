3844d7c4318edd0cdf8550a4c13bff9c6a211106
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2017-03-29 09:33:04 -0300

    first commit

---
+++ src/main/java/clasesDAOHiberJPA/CarteleraDAOHiberJPA.java
@@ -0,0 +1,35 @@
+package clasesDAOHiberJPA;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import Daos.CarteleraDAO;
+import clases.Cartelera;
+@Repository
+public class CarteleraDAOHiberJPA<T> extends GenericoDAOHibernateJPA<Cartelera> implements CarteleraDAO {
+	
+	public CarteleraDAOHiberJPA() {
+		this.persistentClass = Cartelera.class;		
+	}
+	
+	
+	@Override
+	public List<Cartelera> recuperar(){
+		
+		
+		Query consulta= em.createQuery("from Cartelera c where c.padre is null");
+		
+		List<Cartelera> resultado = consulta.getResultList();
+		if (resultado.isEmpty()){
+			return null;
+		}
+		return resultado;	
+
+	}
+
+	
+}
